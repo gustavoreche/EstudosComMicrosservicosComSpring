cf759c9e28fe28b602f83fd9a774fc8d6670e92e
gustavoreche <gustavodreche@msn.com>
2022-01-24 22:07:09 -0300

    Atualiza configs para subir dois servicos e se integrarem

--- estudo-loja/src/main/java/br/com/estudoloja/model/fornecedor/InfoFornecedorDTO.java
+++ estudo-loja/src/main/java/br/com/estudoloja/model/fornecedor/InfoFornecedorDTO.java
@@ -3,6 +3,9 @@ package br.com.estudoloja.model.fornecedor;
 public class InfoFornecedorDTO {
 	
 	private String endereco;
+	
+	public InfoFornecedorDTO() {
+	}
 
 	public InfoFornecedorDTO(String endereco) {
 		this.endereco = endereco;
