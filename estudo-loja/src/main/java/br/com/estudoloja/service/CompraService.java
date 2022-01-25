bcc437a00386b69c14bdf45a40c8a48198b68c80
gustavoreche <gustavodreche@msn.com>
2022-01-25 17:28:48 -0300

    Implementa Eureka Server e Config Server

--- estudo-loja/src/main/java/br/com/estudoloja/service/CompraService.java
+++ estudo-loja/src/main/java/br/com/estudoloja/service/CompraService.java
@@ -10,11 +10,16 @@ import br.com.estudoloja.model.loja.CompraDTO;
 
 @Service
 public class CompraService {
+	
+	private RestTemplate client;
+	
+	public CompraService(RestTemplate client) {
+		this.client = client;
+	}
 
 	public void realizaCompra(CompraDTO compra) {
-		RestTemplate client = new RestTemplate();
-		ResponseEntity<InfoFornecedorDTO> response = client
-				.exchange("http://localhost:8092/info/" + compra.getEndereco().getSiglaEstado(),
+		ResponseEntity<InfoFornecedorDTO> response = this.client
+				.exchange("http://fornecedor/info/" + compra.getEndereco().getSiglaEstado(),
 				HttpMethod.GET, null, InfoFornecedorDTO.class);
 		
 		System.out.println(response.getBody().getEndereco());
